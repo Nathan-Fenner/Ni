cfae1e34f67cd23281f9603e3107f6246106009f
Nathan Fenner <nfenneremail@gmail.com>
2015-07-17 00:24:55 -0700

    Clean up type checking

--- Verify.hs
+++ Verify.hs
@@ -1,41 +1,35 @@
 
-module Verify(verifyAll, Verify(Success, Failure)) where
+module Verify where
 
+import Control.Applicative
 import Expression
 import ParseType
 import Lex
 
 data Verify = Success | Failure [(Token, String)]
 
-(&&&) :: Verify -> Verify -> Verify
-Success &&& msg = msg
-msg &&& Success = msg
-Failure xs &&& Failure ys = Failure (xs ++ ys)
+data Message = Message Token String deriving Show
 
-(.&&&.) :: (Expression -> Verify) -> (Expression -> Verify) -> (Expression -> Verify)
-(f .&&&. g) x = f x &&& g x
+data Check a = Flunk [Message] | Pass a deriving Show
 
-map_ :: (a -> Verify) -> [a] -> Verify
-map_ f xs = go xs where
-	go [] = Success
-	go (x:xs) = f x &&& go xs
+instance Functor Check where
+	fmap f (Pass x) = Pass (f x)
+	fmap f (Flunk msg) = Flunk msg
 
+instance Applicative Check where
+	pure x = Pass x
+	Pass f <*> Pass x = Pass (f x)
+	Flunk m <*> Flunk m' = Flunk (m ++ m')
+	Flunk m <*> _ = Flunk m
+	_ <*> Flunk m = Flunk m
 
-{-
-data Statement
-	= StatementAssign Token Expression
-	| StatementVarVoid Token Type
-	| StatementVarAssign Token Type Expression
-	| StatementDo Expression
-	| StatementIf Token Expression [Statement]
-	| StatementIfElse Token Expression [Statement] [Statement]
-	| StatementWhile Token Expression [Statement]
-	| StatementReturn Token (Maybe Expression)
-	| StatementBreak Token
-	| StatementLet Token [Statement]
-	| StatementFunc { funcToken :: Token, funcName :: Token, argumentsStatement :: [(Token, Type)], funcBangStatement :: Maybe Token, returnTypeStatement :: Maybe Type , bodyStatement :: [Statement] }
-	deriving Show
--}
+instance Monad Check where
+	return = pure
+	Pass x >>= f = f x
+	Flunk m >>= _ = Flunk m
+
+flunk :: Token -> String -> Check any
+flunk token message = Flunk [Message token message]
 
 statementAt :: Statement -> Token
 statementAt (StatementAssign t _) = t
@@ -50,22 +44,6 @@ statementAt (StatementBreak t) = t
 statementAt (StatementLet t _) = t
 statementAt (StatementFunc {funcToken = t}) = t
 
-
-{-
-data Expression
-	= ExpressionIdentifier Token
-	| ExpressionIntegerLiteral Token
-	| ExpressionDecimalLiteral Token
-	| ExpressionStringLiteral Token
-	| ExpressionBang Token
-	| ExpressionCall Expression [Expression]
-	| ExpressionFunc { anonFuncToken :: Token, arguments :: [(Token, Type)], funcBang :: Maybe Token, returnType :: Maybe Type , body :: [Statement] }
-	| ExpressionOp Expression Token Expression
-	| ExpressionPrefix Token Expression
-	deriving Show
--}
-
-
 expressionAt :: Expression -> Token
 expressionAt (ExpressionIdentifier t) = t
 expressionAt (ExpressionIntegerLiteral t) = t
@@ -76,41 +54,6 @@ expressionAt (ExpressionCall e _) = expressionAt e
 expressionAt (ExpressionFunc { anonFuncToken = t }) = t
 expressionAt (ExpressionOp left _ _) = expressionAt left
 expressionAt (ExpressionPrefix op _) = op
-
-levels :: (Expression -> Verify) -> (Statement -> Verify) -> Either Expression Statement -> Verify
-levels f g x = (case x of Left x' -> goe x'; Right x' -> gos x') where
-	goe :: Expression -> Verify
-	goe x = f x &&& goe' x
-	gos :: Statement -> Verify
-	gos x = g x &&& gos' x
-	goe' :: Expression -> Verify
-	goe' (ExpressionCall fun args) = goe fun &&& map_ goe args
-	goe' ExpressionFunc{} = Success -- TODO: body
-	goe' (ExpressionOp left _ right) = goe left &&& goe right
-	goe' (ExpressionPrefix _ arg) = goe arg
-	goe' _ = Success
-	gos' :: Statement -> Verify
-	gos' (StatementAssign _ v) = goe v
-	gos' (StatementVarAssign _ _ v) = goe v
-	gos' (StatementDo e) = goe e
-	gos' (StatementIf _ c body) = goe c &&& map_ gos body
-	gos' (StatementIfElse _ c b1 b2) = goe c &&& map_ gos b1 &&& map_ gos b2
-	gos' (StatementWhile _ c b) = goe c &&& map_ gos b
-	gos' (StatementReturn _ (Just e)) = goe e
-	gos' (StatementLet _ b) = map_ gos b
-	gos' (StatementFunc{bodyStatement = b}) = map_ gos b
-	gos' _ = Success
-
-letVerifier :: Statement -> Verify
-letVerifier (StatementLet _ body) = map_ verifyKind body where
-	verifyKind StatementVarAssign{} = Success
-	verifyKind StatementAssign{} = Success
-	verifyKind StatementFunc{} = Success
-	verifyKind s = Failure [(statementAt s, "let-blocks may contain only assignments, variable declarations with assignment, or function definitions.")]
-letVerifier _ = Success -- skip over all non-let statements
-
-verifyAll :: Either Expression Statement -> Verify
-verifyAll = levels (const Success) letVerifier
 
 data Scope = Scope Type {- return type -} [(Token, Type)] {- stack & globals -} deriving Show
 
@@ -129,113 +72,147 @@ addTypes ((n, t) : rest) scope = addTypes rest $ addType n t scope
 mustReturn :: Scope -> Type
 mustReturn (Scope r _) = r
 
-setReturn :: Scope -> Type -> Scope
-setReturn (Scope _ ts) r = Scope r ts
+setReturn :: Type -> Scope -> Scope
+setReturn r (Scope _ ts) = Scope r ts
 
-x &&&>>> (a, b) = (x &&& a, b)
-(x, _) .&&&>>> (a, b) = (x &&& a, b)
+assertTypeEqual :: Type -> Type -> Token -> Check ()
+assertTypeEqual left right at
+	|left === right = Pass ()
+	|otherwise = flunk at $ "got type `" ++ show left ++ "` but expected type `" ++ show right ++ "`"
 
-{-
-data Expression
-	| ExpressionOp Expression Token Expression
-	| ExpressionPrefix Token Expression
-	deriving Show
--}
-
-
-getExpressionType :: Scope -> Expression -> Either Verify Type
+getExpressionType :: Scope -> Expression -> Check Type
 getExpressionType scope (ExpressionIdentifier name) = case getType name scope of
-	Nothing -> Left $ Failure [(name, "variable `" ++ token name ++ "` has not been declared or is not in scope")]
-	Just t -> Right t
-getExpressionType scope (ExpressionIntegerLiteral{}) = Right $ makeType "Int"
-getExpressionType scope (ExpressionDecimalLiteral{}) = Right $ makeType "Float"
-getExpressionType scope (ExpressionStringLiteral{}) = Right $ makeType "String"
-getExpressionType scope (ExpressionCall fun args) = case funType of
-	Left f -> Left f
-	Right funType' -> matchFuncType funType' argTypes
+	Nothing -> flunk name $ "variable `" ++ token name ++ "` has not been declared or is not in scope"
+	Just t -> Pass t
+getExpressionType scope (ExpressionIntegerLiteral _) = Pass (makeType "Int")
+getExpressionType scope (ExpressionDecimalLiteral _) = Pass (makeType "Float")
+getExpressionType scope (ExpressionStringLiteral _) = Pass (makeType "String")
+getExpressionType scope (ExpressionCall fun args) = do
+	funType <- getExpressionType scope fun
+	matchFuncType funType args
 	where
-	funType :: Either Verify Type
-	funType = getExpressionType scope fun
-	argTypes :: [(Expression, Either Verify Type)]
-	argTypes = zip args $ map (getExpressionType scope) args
-	matchFuncType :: Type -> [(Expression, Either Verify Type)] -> Either Verify Type
-	matchFuncType t [] = Right $ t
-	matchFuncType (TypeBangArrow right) ((ExpressionBang{}, _) : rest) = matchFuncType right rest
-	matchFuncType (TypeBangArrow _) ((arg, argType) : _) = Left $ Failure [(expressionAt arg, "expected bang `!` but got expression " ++ show arg)]
-	matchFuncType (TypeArrow left _) ((ExpressionBang bang, _) : rest) = Left $ Failure [(bang, "was expecting an argument of type " ++ show left ++ ", not a bang `!`")]
-	matchFuncType (TypeArrow left right) ((arg, argType) : rest) = case argType of
-		Left f -> Left f
-		Right argType' -> case argType' === left of
-			True -> matchFuncType right rest
-			False -> Left $ Failure [(expressionAt arg, "expected type " ++ show left ++ " but expression " ++ show arg ++ " with type " ++ show argType' ++ " was applied instead")]
-getExpressionType scope (ExpressionBang bang) = Left $ Failure [(bang, "a lone bang `!` is not a valid expression")]
-getExpressionType (ExpressionFunc funcToken args bang returns body) = undefined -- TODO: verify body + build function type
-
-
-verifyExpressionTypeIs :: Scope -> Expression -> Type -> Verify
-verifyExpressionTypeIs scope (ExpressionIdentifier name) expected = case getType name scope of
-	Nothing -> Failure [(name, "variable `" ++ token name ++ "` has not been declared or is not in scope")]
-	Just t -> if t === expected then Success else Failure [(name, "variable `" ++ token name ++ "` has type " ++ show t ++ " but type " ++ show expected ++ " was expected")]
-verifyExpressionTypeIs scope (ExpressionIntegerLiteral int) expected = case expected === makeType "Int" of
-	True -> Success
-	False -> Failure [(int, "integer `" ++ show int ++ "` has type Int but " ++ show expected ++ " was expected")]
-verifyExpressionTypeIs scope (ExpressionDecimalLiteral float) expected = case expected === makeType "Float" of
-	True -> Success
-	False -> Failure [(float, "float `" ++ show float ++ "` has type Float but " ++ show expected ++ " was expected")]
-verifyExpressionTypeIs scope (ExpressionStringLiteral string) expected = case expected === makeType "String" of
-	True -> Success
-	False -> Failure [(string, "string `" ++ show string ++ "` has type String but " ++ show expected ++ " was expected")]
-verifyExpressionTypeIs scope (ExpressionBang bang) expected = Failure [(bang, "found bang `!` where type " ++ show expected ++ " was expected")]
-verifyExpressionTypeIs scope (ExpressionCall left []) expected = verifyExpressionTypeIs scope left expected
-
-verifyExpressionTypeIs scope expression expected = error "unimplemented expression type"
-
-verifyExpressionTypeConsistent :: Scope -> Expression -> Verify
-verifyExpressionTypeConsistent scope expression = error "unimplemented expression type (consistency)"
-
-verifyStatementType :: Scope -> Statement -> (Verify, Scope)
-verifyStatementType scope (StatementVarVoid nameToken varType) = (Success, addType nameToken varType scope)
-verifyStatementType scope (StatementAssign nameToken value) = (case getType nameToken scope of
-	Nothing -> Failure $ [(nameToken, "variable has not been declared or is not in scope")]
-	Just t -> verifyExpressionTypeIs scope value t, scope)
-verifyStatementType scope (StatementVarAssign nameToken varType value) = (verifyExpressionTypeIs scope value varType, addType nameToken varType scope)
-verifyStatementType scope (StatementDo expression) = (verifyExpressionTypeConsistent scope expression, scope)
-verifyStatementType scope (StatementIf _ condition body)
-	= verifyExpressionTypeIs scope condition (TypeName $ Token "Bool" (FileEnd "*") Identifier)
-	&&&>>> verifyBlockType scope body
-verifyStatementType scope (StatementIfElse _ condition body elseBody)
-	= verifyExpressionTypeIs scope condition (TypeName $ Token "Bool" (FileEnd "*") Identifier)
-	&&&>>> verifyBlockType scope body
-	.&&&>>> verifyBlockType scope elseBody
-verifyStatementType scope (StatementWhile _ condition body)
-	= verifyExpressionTypeIs scope condition (TypeName $ Token "Bool" (FileEnd "*") Identifier)
-	&&&>>> verifyBlockType scope body
-verifyStatementType scope (StatementReturn returnToken Nothing) = case mustReturn scope of
-	TypeName (Token "Void" _ _) -> (Success, scope)
-	t -> (Failure [(returnToken, "expected to return type " ++ show (mustReturn scope) ++ " but returned nothing")], scope)
-verifyStatementType scope (StatementReturn returnToken (Just expected)) = 
-	(verifyExpressionTypeIs scope expected (mustReturn scope), scope)
-verifyStatementType scope (StatementFunc funcToken funcName arguments bang returns body) = (checked, newScope)
+	matchFuncType :: Type -> [Expression] -> Check Type
+	matchFuncType t [] = Pass t
+	matchFuncType (TypeBangArrow right) (ExpressionBang{} : rest) = matchFuncType right rest
+	matchFuncType (TypeBangArrow _) (arg : _) = do
+		flunk (expressionAt arg) $ "expected bang `!` but got expression " ++ show arg
+	matchFuncType (TypeArrow left _) (ExpressionBang bang : rest) =
+		flunk bang $ "was expecting an argument of type " ++ show left ++ ", not a bang `!`"
+	matchFuncType (TypeArrow left right) (arg : rest) = do
+		argType <- getExpressionType scope arg
+		assertTypeEqual argType left (expressionAt arg)
+		matchFuncType right rest
+getExpressionType scope (ExpressionBang bang) = flunk bang "a bang `!` outside of a matching function call is not allowed"
+getExpressionType scope (ExpressionFunc funcToken args bang returns body) = do
+	-- TODO: check the body (after bringing the arguments into scope)
+	Pass funcType
 	where
-	(checked, _) = verifyBlockType (addTypes arguments $ setReturn newScope inferReturns) body
-	newScope = addType funcName funcType scope
-	inferReturns = case returns of Nothing -> TypeName (Token "Void" (FileEnd "*") Identifier); Just t -> t
-	funcType = funcType' (map snd arguments ++ [inferReturns])
-	funcType' [t] = t
-	funcType' (t:ts) = TypeArrow t (funcType' ts)
-verifyStatementType scope (StatementBreak _) = (Success, scope)
-verifyStatementType scope (StatementLet _ body) = verifyBlockType (addTypes newVars scope) body
+	funcType = go (map snd args) returns where
+		go [] Nothing = makeType "Void"
+		go [] (Just r) = r
+		go (t : ts) r = t `TypeArrow` go ts r
+getExpressionType scope (ExpressionOp left op right) = do
+	leftType <- getExpressionType scope left
+	rightType <- getExpressionType scope right
+	assertTypeEqual leftType leftExpect (expressionAt left)
+	assertTypeEqual rightType rightExpect (expressionAt right)
+	return $ returns
 	where
-	newVars = onlyJust $ map varForm body
-	varForm (StatementVarVoid varName varType) = Just (varName, varType)
-	varForm (StatementVarAssign varName varType _) = Just (varName, varType)
-	varForm _ = Nothing
-	onlyJust [] = []
-	onlyJust (Just x : xs) = x : onlyJust xs
-	onlyJust (_ : xs) = onlyJust xs
--- verifyStatementType scope statement = error $ "Statement type not implemented: " ++ show statement
+	triple t = (makeType t, makeType t, makeType t)
+	arithmetic = triple "Int"
+	boolean = triple "Bool"
+	(leftExpect, rightExpect, returns) = case token op of
+		"+" -> arithmetic
+		"-" -> arithmetic
+		"*" -> arithmetic
+		"/" -> arithmetic
+		"%" -> arithmetic
+		"&&" -> boolean
+		"||" -> boolean
+		"++" -> triple "String"
+		_ -> error $ "compiler error: undefined infix operator `" ++ token op ++ "` (please contact the repository owner at github.com/Nathan-Fenner/Ni)"
+getExpressionType scope (ExpressionPrefix op arg) = do
+	argType <- getExpressionType scope arg
+	assertTypeEqual argType expect (expressionAt arg)
+	return returns
+	where
+	(expect, returns) = case token op of
+		"-" -> (makeType "Int", makeType "Int")
+		_ -> error $ "compiler error: undefined prefix operator `" ++ token op ++ "` (please contact the repository owner at github.com/Nathan-Fenner/Ni)"
 
-verifyBlockType :: Scope -> [Statement] -> (Verify, Scope)
-verifyBlockType scope (statement : rest) = let (state, scope') = verifyStatementType scope statement in state &&&>>> verifyBlockType scope' rest
-verifyBlockType scope [] = (Success, scope)
+verifyStatementType :: Scope -> Statement -> Check Scope
+verifyStatementType scope (StatementVarVoid nameToken varType) = return $ addType nameToken varType scope
+verifyStatementType scope (StatementVarAssign nameToken varType value) = do
+	valueType <- getExpressionType scope value
+	assertTypeEqual varType valueType (expressionAt value)
+	return $ addType nameToken varType scope
+verifyStatementType scope (StatementAssign nameToken value) = do
+	valueType <- getExpressionType scope value
+	case getType nameToken scope of
+		Nothing -> flunk nameToken $ "variable `" ++ token nameToken ++ "` is undeclared or out of scope"
+		Just expect -> do
+			assertTypeEqual valueType expect (expressionAt value)
+			return scope
+verifyStatementType scope (StatementDo value) = do
+	_ <- getExpressionType scope value
+	return scope
+verifyStatementType scope (StatementIf _ condition body) = do
+	conditionType <- getExpressionType scope condition
+	assertTypeEqual (makeType "Bool") conditionType (expressionAt condition)
+	verifyStatementBlock scope body
+	return scope
+verifyStatementType scope (StatementIfElse _ condition bodyThen bodyElse) = do
+	conditionType <- getExpressionType scope condition
+	assertTypeEqual (makeType "Bool") conditionType (expressionAt condition)
+	verifyStatementBlock scope bodyThen
+	verifyStatementBlock scope bodyElse
+	return scope
+verifyStatementType scope (StatementWhile _ condition body) = do
+	conditionType <- getExpressionType scope condition
+	assertTypeEqual (makeType "Bool") conditionType (expressionAt condition)
+	verifyStatementBlock scope body
+	return scope
+verifyStatementType scope (StatementReturn returnToken Nothing) = do
+	assertTypeEqual (mustReturn scope) (makeType "Void") returnToken
+	return scope
+verifyStatementType scope (StatementReturn returnToken (Just value)) = do
+	valueType <- getExpressionType scope value
+	assertTypeEqual (mustReturn scope) valueType (expressionAt value)
+	return scope
+verifyStatementType scope (StatementFunc _funcToken funcName arguments _bang returns body) = do
+	let scopeNew = addType funcName funcType scope
+	let scopeBody = setReturn returnType $ addTypes arguments scopeNew
+	verifyStatementBlock scopeBody body
+	return scopeNew
+	where
+	returnType = case returns of
+		Nothing -> makeType "Void"
+		Just t -> t
+	funcType = go (map snd arguments) where
+		go [] = returnType
+		go (t:ts) = t `TypeArrow` go ts
+verifyStatementType scope StatementBreak{} = return scope
+verifyStatementType scope (StatementLet _ body) = do
+	letBody <- mapM verifyLet body
+	-- TODO: prevent duplicate names
+	let newScope = addTypes (concat letBody) scope
+	_ <- mapM (verifyStatementType newScope) body
+	return newScope
+	where
+	verifyLet s@StatementAssign{} = Pass []
+	verifyLet s@(StatementVarAssign varName varType _) = Pass [(varName, varType)]
+	verifyLet s@(StatementFunc _funcToken funcName arguments _bang returns body) = Pass [(funcName, funcType)] where
+		funcType = go (map snd arguments) where
+			go [] = returnType
+			go (t:ts) = t `TypeArrow` go ts
+		returnType = case returns of
+			Nothing -> makeType "Void"
+			Just t -> t
+	verifyLet s = flunk (statementAt s) $ "only assignments, variable definitions, and function declarations are allowed in let blocks"
+	
 
+verifyStatementBlock :: Scope -> [Statement] -> Check Scope
+verifyStatementBlock scope [] = return scope
+verifyStatementBlock scope (s : ss) = do
+	scope' <- verifyStatementType scope s
+	verifyStatementBlock scope' ss
