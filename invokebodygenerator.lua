529f636d689abcf4991cb8eeb0aa7f421663dc04
Curtis Fenner <cfenneremail@gmail.com>
2015-07-30 23:43:31 -0700

    add invokebody.c lua generator

---
+++ invokebodygenerator.lua
@@ -0,0 +1,31 @@
+print("\t// abandon hope, all ye who enter here")
+print("\t// TODO: eliminate this mess")
+print("\tswitch (argc) {")
+for i = 0, 127 do
+	io.write("\tcase " .. i .. ":;\n\t\t")
+
+	io.write("Value (*fn" .. i .. ")(")
+	for j = 1, i do
+		if j > 1 then
+			io.write(", ")
+		end
+		io.write("Value")
+	end
+	io.write(") = (Value(*)(")
+	for j = 1, i do
+		if j > 1 then
+			io.write(", ")
+		end
+		io.write("Value")
+	end
+	io.write("))function.data.function;\n")
+	io.write("\t\treturn fn" .. i .. "(")
+	for j = 1, i do
+		if j > 1 then
+			io.write(", ")
+		end
+		io.write("argv[" .. (j - 1) .. "]")
+	end
+	print(");")
+end
+print("\t}")
